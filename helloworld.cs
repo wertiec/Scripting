b9f6e79298fd88c30d0f82201a9448bac3ea25ac
wertiec <117523052+wertiec@users.noreply.github.com>
2022-11-05 16:32:43 +0300

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //вывод в консоль приветствие миру
+        print("Hello, World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
